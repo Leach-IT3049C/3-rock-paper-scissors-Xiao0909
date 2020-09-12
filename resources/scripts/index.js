29136ebf62d5e4aef953713833bfd4bbd47de43b
Xiao Xiao <49316508+Xiao0909@users.noreply.github.com>
2020-09-11 23:51:16 -0400

    index redo

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,6 +1,52 @@
+// // Elements
+// const welcomeScreen = document.getElementById(`welcome-screen`);
+// const gameScreen = document.getElementById('game-screen');
+// const startGameButton = document.getElementById('start-game-button');
+// const userName = document.getElementById('username');
+// const userSelection = document.getElementById('user-selection');
+// const goButton = document.getElementById('go-button');
+// const scoreParagraph = document.getElementById('score');
+// const gameHistoryParagraph = document.getElementById('game-history');
+
+// // instantiate the game object from the `RockPaperScissors` class.
+// let game;
+
+// // hide gamescreen
+// gameScreen.classList.add(`d-none`);
+
+// // updateScoreTallyUI
+// function updateScoreTallyUI(){
+//   scoreParagraph.innerHTML = userName + ":" + game.score.user + "v CPU" + game.score.cpu;
+// }
+
+// // updateGameHistoryUI
+// function updateGameHistoryUI(){
+//   gameHistoryParagraph.innerHTML += `${game.gameHistoryLog[count]} <br/>`;
+//   gameHistoryParagraph.innerText = game.gameHistorylog;
+// }
+
+// // start-game-button EventListener
+// startGameButton.addEventListener(`click`, function () {
+//   const username = userName.value;
+//   game = new RockPaperScissors(userName);
+//   welcomeScreen.classList.add(`d-none`);
+//   gameScreen.classList.remove(`d-none`);
+//   // Complete
+// });
+
+// // go-button EventListener
+// goButton.addEventListener(`click`, function () {
+//   let userselection = userSelection.value;
+//   game.play(userselection);
+//   count++;
+//   updateScoreTallyUI();
+//   updateGameHistoryUI();
+  
+// });
+
 // Elements
 const welcomeScreen = document.getElementById(`welcome-screen`);
-const gameScreen = document.getElementById('game-screen');
+const gameScreen = document.getElementById('game-screen') ;
 const startGameButton = document.getElementById('start-game-button');
 const userName = document.getElementById('username');
 const userSelection = document.getElementById('user-selection');
@@ -21,25 +67,24 @@ function updateScoreTallyUI(){
 
 // updateGameHistoryUI
 function updateGameHistoryUI(){
-  gameHistoryParagraph.innerHTML += `${game.gameHistoryLog[count]} <br/>`;
-  gameHistoryParagraph.innerText = game.gameHistorylog;
+  gameHistoryParagraph.innerHTML += '${game.gameHistoryLog[count]}<br/>';
+  gameHistoryParagraph.innerText = game.gameHistoryLog;
 }
 
 // start-game-button EventListener
 startGameButton.addEventListener(`click`, function () {
   const username = userName.value;
   game = new RockPaperScissors(userName);
-  welcomeScreen.classList.add(`d-none`);
-  gameScreen.classList.remove(`d-none`);
   // Complete
+  welcomeScreen.classList('d-done');
+  gameScreen.classList.remove('d-done');
 });
 
 // go-button EventListener
-goButton.addEventListener(`click`, function () {
+startGameButton.addEventListener(`click`, function () {
   let userselection = userSelection.value;
   game.play(userselection);
   count++;
   updateScoreTallyUI();
   updateGameHistoryUI();
-  
 });
